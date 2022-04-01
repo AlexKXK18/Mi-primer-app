626e295eaef78dce4f9c55a4398114ecc7926afe
AlexKXK18 <102831683+AlexKXK18@users.noreply.github.com>
2022-04-01 14:19:51 -0700

    agregue link

--- MyApp.py
+++ MyApp.py
@@ -2,3 +2,6 @@ import streamlit as st
 st.title("Mi primer app")
 #st.button("Dale click")
 #st.button("otro boton")
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')

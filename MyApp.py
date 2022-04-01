55504b1e644e3bb6f26487b837317e8c2e369e42
AlexKXK18 <102831683+AlexKXK18@users.noreply.github.com>
2022-04-01 15:04:50 -0700

    Agregue multiplicacion

--- MyApp.py
+++ MyApp.py
@@ -26,3 +26,11 @@ num1 = st.slider('Elige el numero 1', 0.0, 100.0, 25.0)
 num2 = st.slider('Elige el numero 2', 0.0, 100.0, 25.0)
 suma = num1+num2
 st.write("La suma de", num1,"y",num2,"es:",suma)
+
+
+st.write("Ahora multipliqumeos")
+nn1 = st.number_input("dame n1")
+nn2 = st.number_input("dame n2")
+
+mu1=nn1(nn2)
+st.write("la multiplicacion de",nn1 "y", nn2, "es:")

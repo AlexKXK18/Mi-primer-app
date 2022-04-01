dc819d651abdfd0519117eabbe7adc8c3b8742db
AlexKXK18 <102831683+AlexKXK18@users.noreply.github.com>
2022-04-01 15:00:49 -0700

    agregue sliders

--- MyApp.py
+++ MyApp.py
@@ -21,3 +21,8 @@ st.write("El valor de cilck es",Click)
 
 if Click==True:
     st.image("Luffy.jpg")
+
+num1 = st.slider('Elige el numero 1', 0.0, 100.0, 25.0)
+num2 = st.slider('Elige el numero 2', 0.0, 100.0, 25.0)
+suma = num1+num2
+st.write("La suma de", num1,"y",num2,"es:",suma)

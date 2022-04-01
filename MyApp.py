52f0e92a60c7b895b341ce23b088c1339585ff4d
AlexKXK18 <102831683+AlexKXK18@users.noreply.github.com>
2022-04-01 14:33:46 -0700

    Borre el mapa

--- MyApp.py
+++ MyApp.py
@@ -7,6 +7,6 @@ import pandas as pd
 #Las siguintes lineas de cogido eran para visualizar
 #df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 
-3st.write(df)
+#st.write(df)
 
 #st.map(df)
